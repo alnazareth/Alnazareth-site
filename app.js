f192874eacad7473e16a3e72d1c0609aa351daff
alnazareth <36893691+alnazareth@users.noreply.github.com>
2023-11-19 20:54:43 -0400

    My personal site 1.1
    Code of my personal site. No secrets.

---
+++ app.js
@@ -0,0 +1,67 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+const scrollUp = document.querySelector("#scroll-up");
+
+scrollUp.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+
+
+// Nav hamburgerburger selections
+
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+
+burger.addEventListener("click", () => {
+  ul.classList.toggle("show");
+});
+
+// Close hamburger menu when a link is clicked
+
+// Select nav links
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((link) =>
+  link.addEventListener("click", () => {
+    ul.classList.remove("show");
+  })
+);
+
+
+}
+/*
+     FILE ARCHIVED ON 02:30:15 Jun 23, 2022 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 14:28:05 Nov 09, 2023.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 77.353
+  exclusion.robots: 0.072
+  exclusion.robots.policy: 0.062
+  cdx.remote: 0.059
+  esindex: 0.01
+  LoadShardBlock: 47.078 (3)
+  PetaboxLoader3.datanode: 106.698 (5)
+  load_resource: 145.215
+  PetaboxLoader3.resolve: 69.315
+  loaddict: 25.804
+*/
